bb3f9dda9a783b88dfcf42e6c4a8df676da7c4ed
Charlie Rode <charlie.rode1@gmail.com>
2019-07-31 20:45:57 -0700

    edit

--- Lesson_3/05_Serializing data from the database/Starter Code/endpointsproject3.py
+++ Lesson_3/05_Serializing data from the database/Starter Code/endpointsproject3.py
@@ -7,8 +7,8 @@ from models import Base, Puppy
 engine = create_engine('sqlite:///puppies.db')
 Base.metadata.bind = engine
 
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+#DBSession = sessionmaker(bind=engine)
+#session = DBSession()
 
 app = Flask(__name__) 
 
@@ -53,20 +53,32 @@ def puppiesFunctionId(id):
     return deletePuppy(id)
 
 def getAllPuppies():
+  DBSession = sessionmaker(bind=engine)
+  session = DBSession()
+
   puppies = session.query(Puppy).all()
   return jsonify(Puppies=[i.serialize for i in puppies])
 
 def getPuppy(id):
+  DBSession = sessionmaker(bind=engine)
+  session = DBSession()
+
   puppy = session.query(Puppy).filter_by(id = id).one()
   return jsonify(puppy=puppy.serialize) 
   
 def makeANewPuppy(name,description):
+  DBSession = sessionmaker(bind=engine)
+  session = DBSession()
+
   puppy = Puppy(name = name, description = description)
   session.add(puppy)
   session.commit()
   return jsonify(Puppy=puppy.serialize)
 
 def updatePuppy(id,name, description):
+  DBSession = sessionmaker(bind=engine)
+  session = DBSession()
+
   puppy = session.query(Puppy).filter_by(id = id).one()
   if not name:
     puppy.name = name
@@ -77,6 +89,9 @@ def updatePuppy(id,name, description):
   return "Updated a Puppy with id %s" % id
 
 def deletePuppy(id):
+  DBSession = sessionmaker(bind=engine)
+  session = DBSession()
+
   puppy = session.query(Puppy).filter_by(id = id).one()
   session.delete(puppy)
   session.commit()
